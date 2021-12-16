1a1c16933592af36c410075a68c1a334a870b771
PhiX <pokphoenix@gmail.com>
2021-12-16 17:02:19 +0700

    signin and signout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { signIn, signOut } from 'next-auth/react'
 
 const Navbar = () => {
     return ( 
@@ -23,13 +24,19 @@ const Navbar = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link href='#'>
-                        <a>Sign In</a>
+                    <Link href='/api/auth/signin'>
+                        <a onClick={e=>{
+                            e.preventDefault()
+                            signIn('github')
+                        }}>Sign In</a>
                     </Link>
                 </li>
                 <li>
-                    <Link href='#'>
-                        <a>Sign Out</a>
+                    <Link href='/api/auth/signout'>
+                        <a onClick={e=>{
+                            e.preventDefault()
+                            signOut()
+                        }}>Sign Out</a>
                     </Link>
                 </li>
             </ul>

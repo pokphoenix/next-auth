35dd0634b1033837b9424fd639b5e1cfac3de8de
PhiX <pokphoenix@gmail.com>
2021-12-17 15:56:33 +0700

    server-side authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,20 @@
-const Blog = () => {
-    return ( <h1>Blog page</h1> );
+import { getSession, useSession } from "next-auth/react";
+
+const Blog = ({data}) => {
+    const {data:session } = useSession()
+    console.log({session})
+    return ( <h1>Blog page - {data}</h1> );
 }
  
 export default Blog;
+
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+    return {
+        props:{
+            session,
+            data: session ?  'List of 100 perzonalized blogs' : 'List of free blogs'
+        }
+    }
+}
